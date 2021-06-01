8d113c6ff049cd6fc844271ecb7e368ada4d84a5
Sepgey <starberdyev@mail.ru>
2021-06-01 18:22:04 +0300

    Initial commit

---
+++ Philosophers/Philosophers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Philosophers
+{
+    public class Philosopher
+    {
+        private int number;
+        private bool[] availableForks;
+        static Semaphore philosopherHungry = new Semaphore(2, 2);
+        Thread thread;
+
+        public Philosopher(int Number, ref bool[] AvailableForks)
+        {
+            number = Number;
+            availableForks = AvailableForks;
+            thread = new Thread(Eat);
+            thread.Start();
+        }
+
+        public void Eat()
+        {
+            int leftForkIndex = number;
+            int rightForkIndex = number - 1;
+            if (rightForkIndex < 0)
+            {
+                rightForkIndex = 4;
+            }
+
+            Console.WriteLine("Philosopher №" + number + " is thinking");
+
+            if (availableForks[leftForkIndex] == true && availableForks[rightForkIndex] == true)
+            {
+                availableForks[leftForkIndex] = false;
+                availableForks[rightForkIndex] = false;
+                philosopherHungry.WaitOne();
+                Console.WriteLine("Philosopher №" + number + " is eating");
+                philosopherHungry.Release();
+                Console.WriteLine("Philosopher №" + number + "  put forks and begun thinking");
+            }
+        }
+    }
+}
---
+++ Philosophers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Philosophers;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                bool[] forksAvailable = new bool[5] {true, true, true, true, true};
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Philosopher p = new Philosopher(i, ref forksAvailable);
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
