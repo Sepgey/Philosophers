b6738c907f5e923edb277ef664e6f75517cf354a
Sepgey <starberdyev@mail.ru>
2021-06-01 18:29:09 +0300

    Add

--- Philosophers/Program.cs
+++ Philosophers/Program.cs
@@ -9,11 +9,11 @@ namespace Program
         {
             while (true)
             {
-                bool[] forksAvailable = new bool[5] {true, true, true, true, true};
+                bool[] availableForks = new bool[5] {true, true, true, true, true};
 
                 for (int i = 0; i < 5; i++)
                 {
-                    Philosopher p = new Philosopher(i, ref forksAvailable);
+                    Philosopher p = new Philosopher(i, ref availableForks);
                 }
 
                 Thread.Sleep(1000);
